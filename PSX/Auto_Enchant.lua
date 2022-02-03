b35ff0ef01be731c733c034252c09a442af5712c
CandyIsAbsolute <98654154+CandyIsAbsolute@users.noreply.github.com>
2022-02-03 13:44:56 +0800

    uh

--- PSX/Auto_Enchant.lua
+++ PSX/Auto_Enchant.lua
@@ -1,10 +1,12 @@
-local Library, Blacklist, BUID, BankInfo
+
+local Library, Blacklist, BUID, BankInfo, Functions
 do--//Init
     repeat wait(1) until game:IsLoaded() and game:GetService('Players').LocalPlayer ~= nil and game:GetService('Players').LocalPlayer:FindFirstChild('__LOADED')
     Library     = require(game:GetService('ReplicatedStorage').Framework:FindFirstChild('Library'))
+    Functions   = Library.Functions
     Blacklist   = {}
-    BUID = Library.Network.Invoke('Get my Banks')[_G.BankIndex].BUID
-    BankInfo = Library.Network.Invoke('Get Bank', BUID)
+    BUID        = Library.Network.Invoke('Get my Banks')[_G.BankIndex].BUID
+    BankInfo    = Library.Network.Invoke('Get Bank', BUID)
 end
 
 do --//Checks&Functions
@@ -29,6 +31,26 @@ do --//Checks&Functions
         end 
         return temptable
     end
+    function Time(End)
+        local Hours = math.floor(math.floor(End / 60) / 60)
+        local Minutes = math.floor(End / 60)
+        local Seconds = math.floor(End)
+        local MilliSeconds = (End * 100)
+        local Format = ""
+        if Hours > 0 then
+            Format = Format..string.format("%d:", Hours % 60)
+        end
+        if Minutes > 0 then
+            Format = Format..string.format("%d:", Minutes % 60)
+        end
+        if Seconds > 0 then
+            Format = Format..string.format("%d.", Seconds % 60)
+        end
+        if MilliSeconds > 0 then
+            Format = Format..string.format("%dms", MilliSeconds)
+        end
+        return Format
+    end
 end
 
 do--//AutoEnch
@@ -41,23 +63,18 @@ do--//AutoEnch
                     if not Library.Functions.CompareTable(_G.Wanted, PetToValidTable(GetPetInfo(v.uid).powers)) and not HasPower and not _G.Stop then
                         if #GetPetInfo(v.uid).powers > 1 then
                             warn('Pet: '..v.nk..'('..v.uid..')')
-                            warn("      ", GetPetInfo(v.uid).powers[1][1], GetPetInfo(v.uid).powers[1][2])
-                            warn("      ", GetPetInfo(v.uid).powers[2][1], GetPetInfo(v.uid).powers[2][2].."\n------------")
+                            warn("      ", GetPetInfo(v.uid).powers[1][1], Functions.ToRomanNum(GetPetInfo(v.uid).powers[1][2]))
+                            warn("      ", GetPetInfo(v.uid).powers[2][1], Functions.ToRomanNum(GetPetInfo(v.uid).powers[2][2]).."\n------------")
                         else
                             table.foreach(GetPetInfo(v.uid).powers, function(_, __)
                                 warn('Pet: '..v.nk..'('..v.uid..')')
-                                warn("      ",__[1], __[2].."\n------------")
+                                warn("      ",__[1],Functions.ToRomanNum(__[2]).."\n------------")
                             end)
                         end
                         Library.Network.Invoke("Enchant Pet", v.uid)
                     else
-                        local timeTook = tick() - startTime
-                        local mins = math.floor(timeTook / 60)
-                        timeTook = timeTook - (mins * 60)
-                        local secs = math.floor(timeTook)
-                        timeTook = timeTook - secs
-                        local ms = math.floor(timeTook * 1000)
-                        warn('Pet: '..v.nk..'('..v.uid..')'.." has wanted enchants. It took:", string.format("%d Minutes, %d seconds, and %d milliseconds", mins, secs, ms))
+                        local _end = tick() - startTime
+                        warn('Pet: '..v.nk..'('..v.uid..')'.." has wanted enchants. It took:", Time(_end))
                         HasPower = true
                     end
                     if Library.Save.Get().Diamonds < 500000 and _G.Stop ~= true and _G.AutoWithdraw then
@@ -75,4 +92,3 @@ do--//AutoEnch
         end
     end
 end
-
