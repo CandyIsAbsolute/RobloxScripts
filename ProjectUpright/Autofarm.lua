2d1f393ed70cc3535c1063f26e1b2632eb48f004
CandyIsAbsolute <98654154+CandyIsAbsolute@users.noreply.github.com>
2022-04-08 22:36:41 +0800

    Update Autofarm.lua

--- ProjectUpright/Autofarm.lua
+++ ProjectUpright/Autofarm.lua
@@ -1,140 +1,146 @@
+local tweenService = game:GetService("TweenService")
+local vim = game:GetService("VirtualInputManager")
+local ability = game:GetService("ReplicatedStorage"):FindFirstChild("Ability")
+local spawn, wait = task.spawn, task.wait 
+
+local player = game:GetService("Players").LocalPlayer
+local chr = player.Character
+
 
 local library = loadstring(game:HttpGet("https://pastebin.com/raw/ikz1WLPr", true))()
 
-local mainWindow = library:CreateWindow('i love men')
+local npcFarm = library:CreateWindow("NPC Farm")
+--local itemFarm = library:CreateWindow("Item Farm")
 
-local Options = {}
-
-local Enemies = {}
-
-local Abilities = {
-	["Ab_MB1"] = "rep-3",
-	["Ab_E"] = "hold-6",
-	["Ab_R"] = "norm-1",
-	["Ab_T"] = "norm-1",
-	["Ab_F"] = "norm-1",
-	["Ab_H"] = "norm-1",
-	["Ab_Z"] = "norm-1",
+local options = {
+    npcFarm = {
+        abilities = {}
+    },
+    itemFarm = {}
 }
 
-do
-	mainWindow:Section('')
-	do
-		mainWindow:Toggle('Enabled', {
-			location = Options,
-			flag = "enabled"
-		})
-		mainWindow:Bind('Keybind', {
-			location = Options,
-			flag = "keybind",
-			kbonly = true,
-			default = Enum.KeyCode.Semicolon
-		}, function()
-			if typeof(Options.selectedEnemy) ~= "Instance" then 
-				Options.selectedEnemy = Enemies[1]
-				print(typeof(Options.selectedEnemy))
-			end
-			Options.enabled = not Options.enabled
-		end)
-	end
-	mainWindow:Section('')
-	do
-		mainWindow:Dropdown('Enemy', {
-			location = Options,
-			flag = "selectedEnemy",
-			list = Enemies
-		})
-		mainWindow:Slider('Distance', {
-			location = Options,
-			flag = "selectedDistance",
-			min = 1,
-			max = 20,
-			default = 8
-		})
-	end
-	mainWindow:Section('')
-	do
-		for _, v in pairs(Abilities) do
-			if _ ~= nil then
-				mainWindow:Toggle(_:gsub('Ab_', ''), {
-					location = Options,
-					flag = tostring(_)
-				})
-			end
-		end
-	end
-end
-
-do
-	local bVelocity = Instance.new('BodyVelocity')
+do --update stuff
+    local bVelocity = Instance.new("BodyVelocity")
 	bVelocity.MaxForce = Vector3.new()
 	bVelocity.Velocity = Vector3.new()
 	bVelocity.Name = "bV"
-	bVelocity:Clone().Parent = game:GetService('Players').LocalPlayer.Character.HumanoidRootPart
-	task.spawn(function()
-		for _, v in next, game:GetService("Workspace").Alive:GetChildren() do
-			table.insert(Enemies, v)
+    local bAngularVelocity = Instance.new("BodyAngularVelocity")
+    bAngularVelocity.AngularVelocity = Vector3.new()
+    bAngularVelocity.MaxTorque = Vector3.new()
+    bAngularVelocity.Name = "bAV"
+
+	bVelocity:Clone().Parent = chr.HumanoidRootPart
+    bAngularVelocity:Clone().Parent = chr.HumanoidRootPart
+
+    player.CharacterAdded:Connect(function(v)
+
+        chr = v
+        bVelocity:Clone().Parent = v:WaitForChild("HumanoidRootPart", 9e99)
+        bAngularVelocity:Clone().Parent = v:WaitForChild("HumanoidRootPart", 9e99)
+
+    end)
+end
+
+do --npcFarm
+    local enemies = {}
+
+    do--update enemies
+        for _, v in next, game:GetService("Workspace").Alive:GetChildren() do
+			table.insert(enemies, v)
 		end
 		game:GetService("Workspace").Alive.ChildAdded:connect(function(v)
-			table.insert(Enemies, v)
+			table.insert(enemies, v)
 		end)
 		game:GetService("Workspace").Alive.ChildRemoved:connect(function(v)
-			table.remove(Enemies, table.find(Enemies, v))
+			table.remove(enemies, table.find(enemies, v))
 		end)
-		game:GetService('Players').LocalPlayer.CharacterAdded:Connect(function(v)
-			bVelocity:Clone().Parent = v:WaitForChild('HumanoidRootPart')
-		end)
-	end)
-	task.spawn(function()
-		function useAbility()
-			local virtualInputManager = game:GetService('VirtualInputManager')
-			for i, v in next, Abilities do
-				if Options[i] == true then
-					local key = i:gsub('Ab_', '')
-					local todo = tostring(v)
-					if todo:match('norm') then
-						virtualInputManager:SendKeyEvent(true, Enum.KeyCode[key], false, game)
-						task.wait()
-						virtualInputManager:SendKeyEvent(false, Enum.KeyCode[key], false, game)
-					elseif todo:match('rep') then
-						for i = 1, tonumber(todo:split('-')[2]) do
-							virtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
-							task.wait()
-							virtualInputManager:SendMouseButtonEvent(0, 0, 0, false, game, 0)
-						end
-					elseif todo:match('hold') then
-						virtualInputManager:SendKeyEvent(true, Enum.KeyCode[key], false, game)
-						task.wait(tonumber(todo:split('-')[2]))
-						virtualInputManager:SendKeyEvent(false, Enum.KeyCode[key], false, game)
-					end
-				end
-			end
-		end
-		while task.wait() do
-			chr = game:GetService('Players').LocalPlayer.Character
-			if Options.enabled and chr:FindFirstChild('HumanoidRootPart') ~= nil and chr ~= nil then
-				chr.HumanoidRootPart.bV.MaxForce = Vector3.new(1/0, 1/0, 1/0)
-				chr.HumanoidRootPart.CFrame = CFrame.new(Options.selectedEnemy:WaitForChild('HumanoidRootPart').Position.X, Options.selectedEnemy.HumanoidRootPart.Position.Y + Options.selectedDistance, Options.selectedEnemy.HumanoidRootPart.Position.Z) * CFrame.Angles(-math.rad(90), 0, -math.rad(180))
-				task.spawn(useAbility)
-                if not chr:FindFirstChild('Summoned').Value == true then
-                    local virtualInputManager = game:GetService('VirtualInputManager')
-                    repeat
-                        virtualInputManager:SendKeyEvent(true, Enum.KeyCode.Q, false, game)
-                        wait(1)
-                        virtualInputManager:SendKeyEvent(false, Enum.KeyCode.Q, false, game)
-                    until chr:FindFirstChild('Summoned').Value == true and chr:FindFirstChild('Stand').HumanoidRootPart ~= nil
+    end
+
+    npcFarm:Section("")
+    local toggle = npcFarm:Toggle("Enabled", {
+        location = options.npcFarm,
+        flag = "enabled"
+    }, function()
+        task.spawn(startNpcFarm)
+        if options.npcFarm.enabled then
+            chr:FindFirstChild('HumanoidRootPart').bV.MaxForce = Vector3.new(1/0, 1/0, 1/0)
+            chr:FindFirstChild('HumanoidRootPart').bAV.MaxTorque = Vector3.new(1/0, 1/0, 1/0)
+        else
+            chr:FindFirstChild('HumanoidRootPart').bV.MaxForce = Vector3.new()
+            chr:FindFirstChild('HumanoidRootPart').bAV.MaxTorque = Vector3.new()
+        end
+    end)
+
+    npcFarm:Section("")
+    local distance = npcFarm:Slider("Distance", {
+        location = options.npcFarm,
+        flag = "selectedDistance",
+        min = 1, 
+        max = 20,
+        default = 8,
+    })
+    npcFarm:Section("")
+    local enemy = npcFarm:Dropdown("Enemy", {
+        location = options.npcFarm,
+        flag = "selectedEnemy",
+        list = enemies,
+    })
+    npcFarm:Section("")
+    npcFarm:Toggle("MB1", {location = options.npcFarm.abilities, flag = "punch"}):Set(true)
+    npcFarm:Toggle("E", {location = options.npcFarm.abilities, flag = "barrage"}):Set(true)
+    npcFarm:Toggle("R", {location = options.npcFarm.abilities, flag = "R"}):Set(true)
+    npcFarm:Toggle("T", {location = options.npcFarm.abilities, flag = "T"}):Set(true)
+    npcFarm:Toggle("Y", {location = options.npcFarm.abilities, flag = "Y"})
+    npcFarm:Toggle("F", {location = options.npcFarm.abilities, flag = "F"})
+    npcFarm:Toggle("H", {location = options.npcFarm.abilities, flag = "H"})
+    npcFarm:Toggle("J", {location = options.npcFarm.abilities, flag = "J"})
+    npcFarm:Toggle("Z", {location = options.npcFarm.abilities, flag = "Z"})
+    npcFarm:Section("")
+
+    function startNpcFarm()
+        while options.npcFarm.enabled == true do wait()
+            chr = player.Character
+            enemy = options.npcFarm.selectedEnemy
+            if chr ~= nil and enemy:FindFirstChild("HumanoidRootPart") ~= nil then
+                chr.HumanoidRootPart.CFrame = CFrame.new(enemy.HumanoidRootPart.Position.X, enemy.HumanoidRootPart.Position.Y + options.npcFarm.selectedDistance, enemy.HumanoidRootPart.Position.Z) * CFrame.Angles(-math.rad(90), 0, -math.rad(180))
+                
+                if chr:FindFirstChild("Summoned").Value == false then
+                    repeat wait() 
+                        ability:FireServer("Stand Summon", {})
+                    until chr:FindFirstChild("Summoned").Value == true and chr:FindFirstChild('Stand').HumanoidRootPart ~= nil
                 end
-                if 1 > Options.selectedEnemy.Humanoid.Health then
+                useAbilities()
+                if enemy:FindFirstChild('Humanoid') ~= nil and enemy.Humanoid.Health < 1 then
                     for _,v in next, game:GetService("Workspace").Alive:GetChildren() do
-                        if v:WaitForChild('Humanoid').Health > 0 and tostring(v) == tostring(Options.selectedEnemy) then
-                            Options.selectedEnemy = v
+                        if v:WaitForChild('Humanoid').Health > 0 and tostring(v) == tostring(enemy) then
+                            options.npcFarm.selectedEnemy = v
                             break
                         end
                     end
                 end
-			else
-				chr.HumanoidRootPart.bV.MaxForce = Vector3.new()
-			end
-		end
-	end)
+            end
+        end
+
+    end
+    function useAbilities()
+        local function presskey(keyCode, time) 
+            vim:SendKeyEvent(true, Enum.KeyCode[keyCode], false, game)
+            wait(time)
+            vim:SendKeyEvent(false, Enum.KeyCode[keyCode], false, game)
+        end
+        for i, v in next, options.npcFarm.abilities do
+            if v == true then
+                if tostring(i) == "punch" then
+                    for i=1, 3 do
+                        ability:FireServer('Punch', {})
+                        wait()
+                    end
+                elseif tostring(i) == "barrage" then
+                    ability:FireServer('Barrage', {true, 'Hand'})
+                else
+                    presskey(i, 0)
+                end
+            end
+        end
+    end
 end
